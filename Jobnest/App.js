f01938386c647aa86e08b2ac00eda88859e5cfdb
avishka05 <88711826+avishka05@users.noreply.github.com>
2022-11-03 00:27:54 +0530

    creating start page

--- Jobnest/App.js
+++ Jobnest/App.js
@@ -27,6 +27,7 @@ const App = () => {
 
 const styles = StyleSheet.create({
   root: {
+    flex:1,
    
   },
 });
--- Jobnest/src/screens/startScreen/startscreen.js
+++ Jobnest/src/screens/startScreen/startscreen.js
@@ -1,14 +1,25 @@
 /* eslint-disable prettier/prettier */
+
+/* eslint-disable prettier/prettier */
+/* eslint-disable react-hooks/rules-of-hooks */
 //rnfe
-import {View, Text, Image, StyleSheet} from 'react-native'
+import {View, Button ,Text, Image, StyleSheet, useWindowDimensions} from 'react-native';
+
 
 import React from 'react';
-import Logo from '../../../assets/images/jobnestLogo.jpg'
+import Logo from '../../../assets/images/jobnestLogo.jpg';
 const startscreen = () => {
+const {Height} = useWindowDimensions();
+
   return (
     <View style={Styles.root}>
-      <Image source={Logo} style={Styles.logo} resizeMode="contain" />
+      <Image source={Logo} style={[Styles.logo  ,{Height:  Height*0.2}]} resizeMode="contain" />
+      <Text style={Styles.baseText}>Join us & Explore Thousend of</Text>
+
+      <Text style={Styles.baseTex2}>  Greate Job</Text>
+      <Button color="#3E4F88"    title="Get Started"   />
     </View>
+  
   );
 
 };
@@ -17,10 +28,25 @@ const Styles = StyleSheet.create({
     justifyContent:'center',
     alignItems: 'center',
   },
-  logo: {
-    with: 300,
-    height: 400,
+
   
+  logo: {
+    with: '70%',
+    maxWidth:350,
+    maxHeight:400,
+
   },
+  baseText:{
+  
+    fontSize: 16,
+    color: '#3E4F88',
+    
+  },
+  baseTex2:{
+    fontWeight: 'bold',
+    color: '#3E4F88',
+    marginBottom:20,
+  }
+  
 });
 export default startscreen;
